33189917645ebf936b95654558e9820bcfe719c0
0xSeventy <105815325+0xSeventy@users.noreply.github.com>
2023-04-01 21:45:01 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1065,11 +1065,11 @@ def title():
     stdout.write("             " + Fore.LIGHTCYAN_EX + "        ══╦═════════════════════════════════╦══\n")
     stdout.write("             " + Fore.LIGHTCYAN_EX + "╔═════════╩═════════════════════════════════╩═════════╗\n")
     stdout.write(
-        "             " + Fore.LIGHTCYAN_EX + "║ " + Fore.LIGHTWHITE_EX + "        Welcome To The Main Screen Of 0x-DDoS " + Fore.LIGHTCYAN_EX + "     ║\n")
+        "             " + Fore.LIGHTCYAN_EX + "║ " + Fore.LIGHTWHITE_EX + "        Welcome To The Main Screen Of 0x-DDoS " + Fore.LIGHTCYAN_EX + "      ║\n")
     stdout.write(
         "             " + Fore.LIGHTCYAN_EX + "║ " + Fore.LIGHTWHITE_EX + "          Type [help] to see the Commands    " + Fore.LIGHTCYAN_EX + "       ║\n")
     stdout.write(
-        "             " + Fore.LIGHTCYAN_EX + "║ " + Fore.LIGHTWHITE_EX + "         Contact Dev - Telegram @adventurefillop   " + Fore.LIGHTCYAN_EX + "      ║\n")
+        "             " + Fore.LIGHTCYAN_EX + "║ " + Fore.LIGHTWHITE_EX + "         Contact Dev - Telegram @adventurefillop   " + Fore.LIGHTCYAN_EX + " ║\n")
     stdout.write("             " + Fore.LIGHTCYAN_EX + "╚═════════════════════════════════════════════════════╝\n")
     stdout.write("\n")
 
